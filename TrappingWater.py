2190cc086d503f8f6dda72f0e2d3dd06bc5d3302
raviraj <shinderaviraj9@gmail.com>
2025-04-07 00:08:02 +0530

    tapping water

--- TrappingWater.py
+++ TrappingWater.py
@@ -1,1 +1,24 @@
-print('hello world')
+import math
+height=[4,2,0,3,2,5]
+lval=height[0]
+rval=height[len(height)-1]
+lindex=0
+rindex=len(height)-1
+water_hold= [0 for i in range(len(height))]
+print(water_hold)
+left=lindex+1
+right=rindex-1
+
+while left<=right:
+    if lval<=rval :
+       water_hold[left]=max(0,lval-height[left])
+       left=left+1
+    else:
+       water_hold[right]=max(0,rval-height[right])
+       right=right-1
+
+print(water_hold)
+sum_water=sum(water_hold)
+print(sum_water)
+
+
